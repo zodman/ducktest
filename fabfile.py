944020da351d495a60b71edc63749f0bb46716ab
Andres Vargas <andrevargas@coca-cola.com>
2020-05-24 18:59:49 -0500

    fab deploy

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,8 @@ from invoke import run
 
 @task
 def deploy(ctx):
+    run("npm clean-install")
     run("npm run build")
     run("python manage.py collectstatic --noinput")
     rsync(ctx, ".", "app/ducktest", exclude=[".git","node_modules", '.cache'])
+    ctx.run("touch app/ducktest/app/wsgi.py")
