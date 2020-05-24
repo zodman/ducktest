412b325f6c81c6558e3696c6fcedf6c2b6bdfd10
Andres Vargas <andrevargas@coca-cola.com>
2020-05-23 20:55:08 -0500

    delete view for all

--- app/ducks/urls.py
+++ app/ducks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import  path, include
 from django.conf import settings
-from .views import record_list, record_edit, record_add
+from .views import record_list, record_edit, record_add, record_del
 
 urlpatterns = [
     path("list", record_list, name="record_list"),
     path("edit/<int:pk>", record_edit, name="record_edit"),
+    path("delete/<int:pk>", record_del, name="record_del"),
     path("add/", record_add, name="record_add"),
 ]
--- app/ducks/views.py
+++ app/ducks/views.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-from django.views.generic import ListView, UpdateView, CreateView
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django_tables2 import SingleTableView
 import django_tables2 as tables
 from django.urls import reverse_lazy
 from .models import Record
-
 
 class RecordTable(tables.Table):
     actions = tables.TemplateColumn(template_name="ducks/_column_actions.html",
@@ -40,4 +40,13 @@ class RecordCreate(SuccessMessageMixin, CreateView):
 
 record_add = login_required(RecordCreate.as_view())
 
+class RecordDelete(SuccessMessageMixin, DeleteView):
+    model= Record
+    success_url = reverse_lazy("record_list")
+    success_message  = "Record deleted"
 
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
+
+record_del = login_required(RecordDelete.as_view())
