39f79af6fecb42f49596bbb6bdf79690cd60ddf5
Andres Vargas <andrevargas@coca-cola.com>
2020-05-24 19:12:13 -0500

    Replace Bake to fabric

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,18 @@ from fabric import task
 from patchwork.transfers import rsync
 from invoke import run
 
+exclude_dirs =[".git","node_modules", '.cache',".github"]
 
-exclude_dirs = =[".git","node_modules", '.cache',".github"]
+@task
+def reinit(ctx):
+    run("rm db.sqlite3")
+    run("python manage.py migrate")
+    run("python populate.py")
+
+@task
+def test(c):
+    run(" coverage run manage.py test --failfast")
+    run("coverage report -m ")
 
 @task
 def deploy(ctx):
