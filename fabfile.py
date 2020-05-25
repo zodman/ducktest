94dcd73aeb7e86ff7546d74c32540eb11f73f93c
Andres Vargas <andrevargas@coca-cola.com>
2020-05-24 19:06:47 -0500

    use nvm

--- fabfile.py
+++ fabfile.py
@@ -2,10 +2,13 @@ from fabric import task
 from patchwork.transfers import rsync
 from invoke import run
 
+
+exclude_dirs = =[".git","node_modules", '.cache',".github"]
+
 @task
 def deploy(ctx):
     run("npm clean-install")
     run("npm run build")
     run("python manage.py collectstatic --noinput")
-    rsync(ctx, ".", "app/ducktest", exclude=[".git","node_modules", '.cache'])
+    rsync(ctx, ".", "app/ducktest", exclude=exclude_dirs)
     ctx.run("touch app/ducktest/app/wsgi.py")
